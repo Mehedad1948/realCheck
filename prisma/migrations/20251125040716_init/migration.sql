ad32fec13b7acfacf1088605022ea3f4fe4e6a77
Mehrdad <mnourib13@gmail.com>
2025-11-25 07:38:19 +0330

    Initiate database

---
+++ prisma/migrations/20251125040716_init/migration.sql
@@ -0,0 +1,51 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "telegramId" BIGINT NOT NULL,
+    "username" TEXT,
+    "firstName" TEXT,
+    "balance" DOUBLE PRECISION NOT NULL DEFAULT 0.0,
+    "reputation" DOUBLE PRECISION NOT NULL DEFAULT 100.0,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "question" TEXT NOT NULL,
+    "content" TEXT,
+    "imageUrls" TEXT[],
+    "options" TEXT[],
+    "correctAnswer" TEXT,
+    "isValidation" BOOLEAN NOT NULL DEFAULT false,
+    "reward" DOUBLE PRECISION NOT NULL DEFAULT 0.05,
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Vote" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "taskId" TEXT NOT NULL,
+    "selection" TEXT NOT NULL,
+    "isCorrect" BOOLEAN,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Vote_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_telegramId_key" ON "User"("telegramId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Vote_userId_taskId_key" ON "Vote"("userId", "taskId");
+
+-- AddForeignKey
+ALTER TABLE "Vote" ADD CONSTRAINT "Vote_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Vote" ADD CONSTRAINT "Vote_taskId_fkey" FOREIGN KEY ("taskId") REFERENCES "Task"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
