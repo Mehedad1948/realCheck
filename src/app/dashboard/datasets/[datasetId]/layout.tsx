5714d16a036a16999ebbba90d7d6d6b97f19b731
Mehrdad <mnourib13@gmail.com>
2025-11-29 11:53:34 +0330

    Add sussepense boundry

---
+++ src/app/dashboard/datasets/[datasetId]/layout.tsx
@@ -0,0 +1,11 @@
+import { ReactNode, Suspense } from 'react';
+
+export default function layout({ children }: { children: ReactNode }) {
+    return (
+        <div>
+            <Suspense>
+                {children}
+            </Suspense>
+        </div>
+    );
+}
